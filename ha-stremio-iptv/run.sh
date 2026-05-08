08bad53264f1de926be21cb68f6bd1dea411d906
Arta <63342785+dartanidi@users.noreply.github.com>
2026-05-08 19:52:51 +0200

    Create run.sh

---
+++ ha-stremio-iptv/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+CONFIG_PATH=/data/options.json
+
+echo "[Add-on] Lettura configurazione da Home Assistant..."
+
+export M3U_URL=$(jq --raw-output '.m3u_url // empty' $CONFIG_PATH)
+export EPG_URL=$(jq --raw-output '.epg_url // empty' $CONFIG_PATH)
+export REFRESH_INTERVAL_MIN=$(jq --raw-output '.refresh_interval_minutes // 60' $CONFIG_PATH)
+export EASYPROXY_URL=$(jq --raw-output '.easyproxy_url // empty' $CONFIG_PATH)
+export EASYPROXY_PASSWORD=$(jq --raw-output '.easyproxy_password // empty' $CONFIG_PATH)
+
+echo "[Add-on] Avvio server Stremio..."
+exec node /usr/src/app/index.js
